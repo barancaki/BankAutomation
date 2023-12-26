675ab5b5d96123b6d6e221775db0b6c557513d56
Baran Çakı <115949949+barancaki@users.noreply.github.com>
2023-12-26 13:14:07 +0300

    Update Hesap.java
    Kullanıcının hesaba atmak istediği para 0 veya negatif bir değer olamaz eğer böyle bir işlem yapmak isterse karşısına bir uyarı mesajı çıkacaktır.

--- src/Hesap.java
+++ src/Hesap.java
@@ -20,23 +20,32 @@ public class Hesap {
     }
 
     public void paraYatir(double tutar) {
-        bakiye += tutar;
+        if (tutar > 0) {
+            bakiye += tutar;
+            System.out.println(tutar + " TL hesabınıza yatırıldı. Yeni bakiye: " + bakiye + " TL");
+        } else {
+            JOptionPane.showMessageDialog(null, "Geçersiz tutar. Pozitif bir değer giriniz.", "Uyarı", JOptionPane.WARNING_MESSAGE);
+        }
     }
 
     public boolean paraCek(double tutar) {
-        if (tutar > bakiye) {
-            return false;
-        } else {
+        if (tutar > 0 && tutar <= bakiye) {
             bakiye -= tutar;
+            System.out.println(tutar + " TL hesabınızdan çekildi. Yeni bakiye: " + bakiye + " TL");
             return true;
+        } else {
+            JOptionPane.showMessageDialog(null, "Geçersiz tutar. Lütfen uygun bir miktar giriniz.", "Uyarı", JOptionPane.WARNING_MESSAGE);
+            return false;
         }
     }
 
     public boolean krediBasvuru(double gelir, double krediMiktari) {
-        if (gelir >= 1000 && krediMiktari <= gelir * 5) {
+        if (gelir >= 1000 && krediMiktari > 0 && krediMiktari <= gelir * 5) {
             bakiye += krediMiktari;
+            System.out.println("Kredi başvurunuz onaylandı. " + krediMiktari + " TL hesabınıza yatırıldı.");
             return true;
         } else {
+            JOptionPane.showMessageDialog(null, "Kredi başvurunuz reddedildi. Lütfen geçerli bir miktar giriniz.", "Uyarı", JOptionPane.WARNING_MESSAGE);
             return false;
         }
     }
