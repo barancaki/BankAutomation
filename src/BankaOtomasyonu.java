dfa6c52aa300e7a3d5237882aae793e07bfbab81
Baran Çakı <115949949+barancaki@users.noreply.github.com>
2023-12-26 13:23:13 +0300

    Update BankaOtomasyonu.java
    açıklamalar eklendi

--- src/BankaOtomasyonu.java
+++ src/BankaOtomasyonu.java
@@ -4,10 +4,15 @@ import java.util.ArrayList;
 public class BankaOtomasyonu {
 
     public static void main(String[] args) {
+        // Hesapları ve kayıtlı kullanıcıları tutmak için ArrayList'ler oluşturuluyor.
         ArrayList<Hesap> hesaplar = new ArrayList<>();
         ArrayList<Kullanici> kayitliKullanicilar = new ArrayList<>();
+
+        // Kullanıcı arayüzü için JTextArea oluşturuluyor.
         JTextArea sonucArea = new JTextArea();
 
+        // SwingUtilities.invokeLater, arayüz güncellemelerini Swing EDT (Event Dispatch Thread) üzerinde gerçekleştirmek için kullanılır.
+        // RenkliBankaOtomasyonuUI sınıfı, arayüzü başlatmak ve gerekli parametreleri iletmek için kullanılır.
         SwingUtilities.invokeLater(() -> new RenkliBankaOtomasyonuUI(hesaplar, kayitliKullanicilar));
     }
 }
