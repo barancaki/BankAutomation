dadb68a296f96826ba50f1698286a85306dbf971
Baran Çakı <115949949+barancaki@users.noreply.github.com>
2023-12-26 13:25:48 +0300

    Update KullaniciYonetimi.java
    açıklamalar eklendi

--- src/KullaniciYonetimi.java
+++ src/KullaniciYonetimi.java
@@ -1,26 +1,34 @@
 import javax.swing.*;
 import java.util.ArrayList;
 
+// KullaniciYonetimi sınıfı, kayıtlı kullanıcıları yönetir.
 public class KullaniciYonetimi {
 
+    // Kayıtlı kullanıcıların listesi
     private static ArrayList<Kullanici> kayitliKullanicilar = new ArrayList<>();
 
+    // Yeni kullanıcı kaydı oluşturan metot.
     public static void kullaniciKayit(String kullaniciAdi, String sifre, ArrayList<Kullanici> kayitliKullanicilar) {
+        // Eğer kullanıcı daha önce kaydedilmemişse, yeni kullanıcı oluşturulup listeye eklenir.
         if (!kullaniciVarMi(kullaniciAdi)) {
             Kullanici yeniKullanici = new Kullanici(kullaniciAdi, sifre);
             kayitliKullanicilar.add(yeniKullanici);
         }
     }
 
+    // Kullanıcı girişi kontrolü yapan metot.
     public static boolean kullaniciGiris(String kullaniciAdi, String sifre, ArrayList<Kullanici> kayitliKullanicilar) {
+        // Kayıtlı kullanıcılar arasında kullanıcı adı ve şifresi uyan bir kullanıcı varsa true döner.
         for (Kullanici kullanici : kayitliKullanicilar) {
             if (kullanici.getKullaniciAdi().equals(kullaniciAdi) && kullanici.getSifre().equals(sifre)) {
                 return true;
             }
         }
+        // Uygun kullanıcı bulunamazsa false döner.
         return false;
     }
 
+    // Belirtilen kullanıcı adının kayıtlı olup olmadığını kontrol eden yardımcı metot.
     private static boolean kullaniciVarMi(String kullaniciAdi) {
         for (Kullanici kullanici : kayitliKullanicilar) {
             if (kullanici.getKullaniciAdi().equals(kullaniciAdi)) {
