d6b9306042e34df455f7ea7c2e556d5829728a3c
Baran Çakı <115949949+barancaki@users.noreply.github.com>
2023-12-26 13:25:24 +0300

    Update Kullanici.java

--- src/Kullanici.java
+++ src/Kullanici.java
@@ -1,16 +1,22 @@
+// Kullanici sınıfı, banka otomasyonu için kullanıcı bilgilerini temsil eder.
 public class Kullanici {
+
+    // Kullanıcının adı ve şifresi
     private String kullaniciAdi;
     private String sifre;
 
+    // Kullanıcı sınıfının yapıcı metodu, kullanıcı adı ve şifre alarak bir kullanıcı oluşturur.
     public Kullanici(String kullaniciAdi, String sifre) {
         this.kullaniciAdi = kullaniciAdi;
         this.sifre = sifre;
     }
 
+    // Kullanıcının adını getiren metot.
     public String getKullaniciAdi() {
         return kullaniciAdi;
     }
 
+    // Kullanıcının şifresini getiren metot.
     public String getSifre() {
         return sifre;
     }
